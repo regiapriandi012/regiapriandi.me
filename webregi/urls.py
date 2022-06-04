52b646132d7ccea2116e380b5755dea6eb00ce80
Regi Apriandi <regiapriandi012@gmail.com>
2022-06-04 16:28:22 +0700

    bismillah

---
+++ webregi/urls.py
@@ -0,0 +1,10 @@
+from . import views
+from django.urls import path
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('resume/', views.resume, name='resume'),
+    path('blog/', views.PostList.as_view(), name='blog'),
+    #path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('<slug:slug>/', views.post_detail, name='post_detail')
+]
---
+++ webregi/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.views import generic
+from .models import Post
+from .forms import CommentForm
+from django.shortcuts import render, get_object_or_404
+
+def index(request):
+    return render(request, "index.html")
+
+def resume(request):
+    return render(request, "achievement.html")
+
+class PostList(generic.ListView):
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    template_name = 'blog.html'
+
+#class PostDetail(generic.DetailView):
+#    model = Post
+#    template_name = 'blog_detail.html'
+
+def post_detail(request, slug):
+    template_name = 'blog_detail.html'
+    post = get_object_or_404(Post, slug=slug)
+    comments = post.comments.filter(active=True)
+    new_comment = None
+    # Comment posted
+    if request.method == 'POST':
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+
+            # Create Comment object but don't save to database yet
+            new_comment = comment_form.save(commit=False)
+            # Assign the current post to the comment
+            new_comment.post = post
+            # Save the comment to the database
+            new_comment.save()
+    else:
+        comment_form = CommentForm()
+
+    return render(request, template_name, {'post': post,
+                                           'comments': comments,
+                                           'new_comment': new_comment,
+                                           'comment_form': comment_form})
