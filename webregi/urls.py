8de6184d1d6ec696561028b8d554be99d64a2cba
Regi Apriandi <regiapriandi012@gmail.com>
2022-06-05 17:10:28 +0700

    bismillah

--- webregi/urls.py
+++ webregi/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path('blog/', views.PostList.as_view(), name='blog'),
     #path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('ads.txt', AdsView.as_view()),
+    path('ads.txt', views.AdsView.as_view()),
 ]
