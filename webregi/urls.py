faa0b23011c48d16f987bc79c6f4e0d9160740dd
Regi Apriandi <regiapriandi012@gmail.com>
2022-06-05 17:20:04 +0700

    bismillah

--- webregi/urls.py
+++ webregi/urls.py
@@ -7,5 +7,4 @@ urlpatterns = [
     path('blog/', views.PostList.as_view(), name='blog'),
     #path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
-    path('ads.txt', views.AdsView.as_view()),
 ]
--- webregiapriandi/urls.py
+++ webregiapriandi/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from webregi.views import AdsView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('webregi.urls'))
+    path('', include('webregi.urls')),
+    path('ads.txt', views.AdsView.as_view()),
 ]
