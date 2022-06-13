1e2f704227c9d3bd7195b4d29afd83aa21fcdde3
Regi Apriandi <regiapriandi012@gmail.com>
2022-06-13 12:30:49 +0700

    bismillah

--- webregi/models.py
+++ webregi/models.py
@@ -10,6 +10,7 @@ STATUS = (
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
+    image = models.ImageField(upload_to='featured_image/%Y/%m/%d/')
     author = models.ForeignKey(User, on_delete= models.CASCADE,related_name='blog_posts')
     updated_on = models.DateTimeField(auto_now= True)
     content = models.TextField()
