0128f930f87ebd7701905bb590a73b1e6e4a2331
Regi Apriandi <regiapriandi012@gmail.com>
2022-06-13 12:47:49 +0700

    bismillah

--- webregi/models.py
+++ webregi/models.py
@@ -7,7 +7,7 @@ STATUS = (
     (1,"Publish")
 )
 
-class Post(models.Model):
+class Postt(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
     image = models.ImageField(upload_to='featured_image/%Y/%m/%d/')
@@ -29,7 +29,7 @@ class Post(models.Model):
         return reverse("post_detail", kwargs={"slug": str(self.slug)})
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post,on_delete=models.CASCADE,related_name='comments')
+    post = models.ForeignKey(Postt,on_delete=models.CASCADE,related_name='comments')
     name = models.CharField(max_length=80)
     email = models.EmailField()
     body = models.TextField()
