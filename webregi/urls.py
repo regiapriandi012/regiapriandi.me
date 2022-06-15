7ce7bc63febbd1907bbe0c61cb285591fb71af20
Regi Apriandi <regiapriandi012@gmail.com>
2022-06-15 14:54:29 +0700

    bismillah FINALL

--- webregi/urls.py
+++ webregi/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
-from .feeds import LatestPostsFeed
+from .feeds import LatestPostsFeed, AtomSiteNewsFeed
 
 urlpatterns = [
     path('', views.index, name='index'),
