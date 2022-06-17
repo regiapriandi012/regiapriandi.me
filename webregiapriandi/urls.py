381989073943015c0b6f4fab4914977e8740618a
Regi Apriandi <regiapriandi012@gmail.com>
2022-06-17 09:50:34 +0700

    bismillah FINALL

--- webregiapriandi/urls.py
+++ webregiapriandi/urls.py
@@ -12,6 +12,8 @@ sitemaps = {
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}), 
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
     path('', include('webregi.urls')),
     path('ads.txt', AdsView.as_view()),
     path('summernote/', include('django_summernote.urls')),
