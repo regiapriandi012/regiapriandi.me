52008e3664b1facc74eb3b83cb3ccca933c063de
Regi Apriandi <regiapriandi012@gmail.com>
2022-06-17 11:37:19 +0700

    bismillah FINALL

--- webregiapriandi/urls.py
+++ webregiapriandi/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.sitemaps.views import sitemap
 from webregi.sitemaps import PostSitemap
+from django.conf.urls import url
 
 sitemaps = {
     "posts": PostSitemap,
@@ -14,9 +15,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('webregi.urls')),
     path('ads.txt', AdsView.as_view()),
+    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('summernote/', include('django_summernote.urls')),
     path("sitemap.xml", sitemap, {"sitemaps": sitemaps}, name="sitemap"),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+"""if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)"""
