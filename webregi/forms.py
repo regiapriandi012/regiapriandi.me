6ac618777d49826ba39c6136880791c4d29601ce
Regi Apriandi <regiapriandi012@gmail.com>
2022-09-13 10:26:31 +0700

    update forms

--- webregi/forms.py
+++ webregi/forms.py
@@ -1,8 +1,15 @@
 from .models import Comment
 from django import forms
+from captcha.fields import ReCaptchaField
+from captcha.widgets import ReCaptchaV2Checkbox
 
-
-class CommentForm(forms.ModelForm):
+"""class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('name', 'email', 'body')
+        fields = ('name', 'email', 'body')"""
+
+class CommentForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    body = forms.CharField(widget=forms.Textarea)
+    captcha = ReCaptchaField(widget=ReCaptchaV2Checkbox)
